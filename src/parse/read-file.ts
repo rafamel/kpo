1cd195c02e96b96ce8a9c1e620f5ececef1b9a52
Rafa Mel <melrcm@gmx.net>
2019-04-12 01:08:42 +0200

    feat(parse): adds readFile and IScripts type

---
+++ src/parse/read-file.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import pify from 'pify';
+import yaml from 'js-yaml';
+import { IScripts } from '~/types';
+import ensure from '~/utils/ensure';
+
+export default async function readFile(file: string): Promise<IScripts> {
+  const { ext } = path.parse(file);
+
+  switch (ext) {
+    case '.js':
+      return require(file);
+    case '.json':
+      return JSON.parse(await ensure.rejection(() => pify(fs.readFile)(file)));
+    case '.yml':
+    case '.yaml':
+      return yaml.safeLoad(
+        await ensure.rejection(() => pify(fs.readFile)(file))
+      );
+    default:
+      throw Error(`Extension not valid`);
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,13 @@
 import { LogLevelDesc } from 'loglevel';
 
+export interface IOfType<T> {
+  [key: string]: T;
+}
+
 export type TLogger = LogLevelDesc;
+
+export type TScript = string | (() => Promise<void> | void);
+
+export interface IScripts {
+  [key: string]: TScript | TScript[] | IScripts;
+}
