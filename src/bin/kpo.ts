610420e1fc1deee49afd595bdc038624ee37c04c
Rafa Mel <melrcm@gmx.net>
2019-04-20 21:58:51 +0200

    feat(bin/kpo): exists w/ code 1 only if not silent

--- src/bin/kpo.ts
+++ src/bin/kpo.ts
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
 import main from './main';
+import state from '~/state';
 import { error } from 'cli-belt';
 import logger from '~/utils/logger';
 
 main(process.argv.slice(2)).catch((err) => {
-  return error(err, { exit: 1, debug: true, logger });
+  return error(err, {
+    exit: state.get('silent') ? 0 : 1,
+    debug: true,
+    logger
+  });
 });
