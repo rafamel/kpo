657ad241c82cab7f1817bbd473cc8ce4b6ce6866
Rafa Mel <melrcm@gmx.net>
2019-04-20 22:02:41 +0200

    feat(types): allows circular renference as Array and falsy values for TScript

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,16 @@ export interface IOfType<T> {
 
 export type TLogger = LogLevelDesc;
 
-export type TScript = string | (() => Promise<void> | void);
+export type TScript =
+  | undefined
+  | null
+  | false
+  | string
+  | (() => Promise<TScript> | TScript)
+  | IScriptsArray;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IScriptsArray extends Array<TScript> {}
 
 export interface IScripts {
   [key: string]: TScript | TScript[] | IScripts;
