5c548506cc6c0e61c29ec25f5a4ccb83a0643e11
Rafa Mel <melrcm@gmx.net>
2019-04-25 06:29:43 +0200

    feat(core/load): gets scripts key and sets options from options key from kpo scripts file if not a j

--- src/core/load.ts
+++ src/core/load.ts
@@ -4,7 +4,8 @@ import yaml from 'js-yaml';
 import { rejects } from 'errorish';
 import { open } from '~/utils/errors';
 import { ILoaded, IPaths } from './types';
-import { IScripts } from '~/types';
+import { IOfType } from '~/types';
+import options from './options';
 
 export default async function load(paths: IPaths): Promise<ILoaded> {
   return {
@@ -13,23 +14,34 @@ export default async function load(paths: IPaths): Promise<ILoaded> {
   };
 }
 
-export async function loadFile(file: string): Promise<IScripts> {
+export async function loadFile(file: string): Promise<IOfType<any>> {
   const { ext } = path.parse(file);
 
   switch (ext) {
     case '.js':
       return open.throws(() => require(file));
     case '.json':
-      return fs.readJSON(file).catch(rejects);
+      return fs
+        .readJSON(file)
+        .catch(rejects)
+        .then(getScripts);
     case '.yml':
     case '.yaml':
-      return yaml.safeLoad(
+      const kpo = yaml.safeLoad(
         await fs
           .readFile(file)
           .then(String)
           .catch(rejects)
       );
+      return getScripts(kpo);
     default:
-      throw Error(`Extension not valid`);
+      throw Error(`Extension not valid for ${file}`);
   }
+}
+
+export function getScripts(kpo: IOfType<any>): IOfType<any> {
+  if (!kpo.scripts) throw Error(`Scripts file didn't contain a scripts key`);
+
+  if (kpo.options) options.setScope(kpo.options);
+  return kpo.scripts;
 }
