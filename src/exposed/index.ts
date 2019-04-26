76f99e5b06f3d42ed9d55767b9168d56928f6704
Rafa Mel <melrcm@gmx.net>
2019-04-26 04:58:35 +0200

    feat(exposed): adds confirm prompt

--- src/exposed/index.ts
+++ src/exposed/index.ts
@@ -1,3 +1,4 @@
 export * from './exec';
+export * from './prompt';
 export { default as options } from './options';
 export { default as line } from './line';
---
+++ src/exposed/prompt/confirm.ts
@@ -0,0 +1,34 @@
+import { TScript } from '~/types';
+import prompts from 'prompts';
+import { status } from 'promist';
+
+export interface IConfirmOptions {
+  message?: string;
+  initial?: boolean;
+  yes?: TScript;
+  no?: TScript;
+  timeout?: number;
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function confirm(options: IConfirmOptions = {}) {
+  return async function confirm(): Promise<TScript> {
+    const promise = status(
+      prompts({
+        type: 'confirm',
+        name: 'value',
+        message: options.message || 'Continue?',
+        initial: options.initial || false
+      })
+    );
+
+    if (options.timeout) {
+      setTimeout(() => {
+        if (promise.status === 'pending') process.stdin.emit('data', '\n');
+      }, options.timeout);
+    }
+
+    const response = await promise;
+    return (response.value ? options.yes : options.no) || undefined;
+  };
+}
