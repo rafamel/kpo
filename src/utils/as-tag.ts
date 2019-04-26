c494a0805796b3f647723c10630ba1c79566eb94
Rafa Mel <melrcm@gmx.net>
2019-04-27 01:14:57 +0200

    fix(utils/as-tag): only considers arguments as coming from a template literal if their number matche

--- src/utils/as-tag.ts
+++ src/utils/as-tag.ts
@@ -5,6 +5,7 @@ function asTag(literals: TemplateStringsArray, ...placeholders: any[]): string;
 function asTag(...args: any[]): string {
   let literals = args.shift();
   if (!Array.isArray(literals)) literals = [literals || ''];
+  if (args.length !== literals.length - 1) args = [];
 
   let str = '';
   for (let i = 0; i < literals.length; i++) {
