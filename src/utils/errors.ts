dc341f4ff020b0998c49cf95c924d42eaafa750c
Rafa Mel <melrcm@gmx.net>
2019-04-27 01:16:41 +0200

    fix(utils/errors): fixes wrap configuration to always overwrite message

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -4,14 +4,15 @@ export class CustomError<T> extends Errorish<T> {}
 
 export class OpenError<T> extends CustomError<T> {}
 
+export class WrappedError<T> extends CustomError<T> {}
+
 export const open = scope.set('_kpo_open_', {
   Error: CustomError,
   Errorish: OpenError
 });
 
-export class WrappedError<T> extends CustomError<T> {}
-
 export const wrap = scope.set('_kpo_wrap_', {
+  allow: [],
   Error: CustomError,
   Errorish: WrappedError
 });
