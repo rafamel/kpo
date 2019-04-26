9a735f688b2daba143e4e8218ed65b34210d4941
Rafa Mel <melrcm@gmx.net>
2019-04-26 03:22:43 +0200

    feat(exposed): adds parallel

--- src/exposed/index.ts
+++ src/exposed/index.ts
@@ -1,4 +1,5 @@
 export { default as options } from './options';
 export { default as series } from './series';
+export { default as parallel } from './parallel';
 export { default as silent } from './silent';
 export { default as line } from './line';
---
+++ src/exposed/parallel.ts
@@ -0,0 +1,49 @@
+import core from '~/core';
+import { IExecOptions } from '~/types';
+import logger from '~/utils/logger';
+
+export interface IParallelOptions extends IExecOptions {
+  names?: string[];
+  colors?: string[];
+  /**
+   * If `true`, it will fail early, killing all other processes if any of them fails.
+   */
+  early?: boolean;
+  /**
+   * If `true`, it will never throw.
+   */
+  silent?: boolean;
+}
+
+export default function parallel(
+  commands: string | string[],
+  options: IParallelOptions = {}
+) {
+  return async function parallel(args?: string[]): Promise<void> {
+    const argv: string[] = Array.isArray(commands)
+      ? commands.concat()
+      : [commands];
+
+    if (options.names && options.names.length) {
+      argv.push('--names', options.names.join(','));
+    }
+    if (options.colors && options.colors.length) {
+      argv.push('-c', options.colors.join(','));
+    }
+    if (options.early) {
+      argv.push('--kill-others-on-fail');
+    }
+
+    try {
+      await core.exec(
+        require.resolve('concurrently/bin/concurrently'),
+        args ? argv.concat(args) : argv,
+        true,
+        options
+      );
+    } catch (err) {
+      if (options.silent) logger.error(err);
+      else throw err;
+    }
+  };
+}
