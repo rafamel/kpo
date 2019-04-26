3a5191b33923600b58076768ef77c471d2bb60b1
Rafa Mel <melrcm@gmx.net>
2019-04-27 01:17:56 +0200

    feat(utils/errors): adds custom name getter for error classes

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -2,9 +2,17 @@ import { scope, Errorish } from 'errorish';
 
 export class CustomError<T> extends Errorish<T> {}
 
-export class OpenError<T> extends CustomError<T> {}
+export class OpenError<T> extends CustomError<T> {
+  public get name(): string {
+    return 'OpenError';
+  }
+}
 
-export class WrappedError<T> extends CustomError<T> {}
+export class WrappedError<T> extends CustomError<T> {
+  public get name(): string {
+    return 'WrappedError';
+  }
+}
 
 export const open = scope.set('_kpo_open_', {
   Error: CustomError,
