685f4fa200a2a3fff971cc43e7eaa837a78d7d21
Rafa Mel <melrcm@gmx.net>
2019-04-27 01:10:30 +0200

    refactor(exposed/tags): minor refactor for line

--- src/exposed/tags/line.ts
+++ src/exposed/tags/line.ts
@@ -4,7 +4,15 @@ export default line;
 
 function line(str: string): string;
 function line(literals: TemplateStringsArray, ...placeholders: any[]): string;
+/**
+ * String tag; converts a multiple line, indented, string, into a single line string:
+ * ```javascript
+ *  line`kpo foo bar baz
+ *    -- --foo bar --baz foobar`
+ *  // 'kpo foo bar baz -- --foo bar --baz foobar'
+ * ```
+ * @returns a `string`
+ */
 function line(...args: any[]): string {
-  // @ts-ignore
-  return oneLine(...args);
+  return oneLine(args.shift(), ...args);
 }
