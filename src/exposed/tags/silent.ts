f0ac3ea10ea4fb455da103f85bd63b876255ef37
Rafa Mel <melrcm@gmx.net>
2019-04-26 09:43:49 +0200

    fix(exposed/tags): fixes silent

--- src/exposed/tags/silent.ts
+++ src/exposed/tags/silent.ts
@@ -1,6 +1,7 @@
 import { TScript } from '~/types';
 import core from '~/core';
 import asTag from '~/utils/as-tag';
+import logger from '~/utils/logger';
 
 export default silent;
 
@@ -9,9 +10,17 @@ function silent(
   literals: TemplateStringsArray,
   ...placeholders: any[]
 ): TScript;
+/**
+ * String tag; executes a command that will always exit with code 0.
+ * @returns A `TScript`, as a function, that won't be executed until called by `kpo` -hence, calling `silent` won't have any effect until the returned function is called.
+ */
 function silent(...args: any[]): TScript {
   return async function silent(argv): Promise<void> {
     const command = asTag(args.shift(), ...args);
-    return core.exec(command, argv, false);
+    try {
+      await core.exec(command, argv, false);
+    } catch (err) {
+      logger.error(err);
+    }
   };
 }
