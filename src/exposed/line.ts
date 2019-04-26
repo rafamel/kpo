9f33f15227c2ab754f56ba49142a80c3494ae5bb
Rafa Mel <melrcm@gmx.net>
2019-04-26 05:30:57 +0200

    fix(exposed/line): fixes tag typings

--- src/exposed/line.ts
+++ src/exposed/line.ts
@@ -3,7 +3,7 @@ import { oneLine } from 'common-tags';
 export default line;
 
 function line(str: string): string;
-function line(literals: string[], ...placeholders: any[]): string;
+function line(literals: TemplateStringsArray, ...placeholders: any[]): string;
 function line(...args: any[]): string {
   // @ts-ignore
   return oneLine(...args);
