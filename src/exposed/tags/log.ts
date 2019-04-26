c9b3a1cec392aa8cd73050146e788ae1fc55401e
Rafa Mel <melrcm@gmx.net>
2019-04-27 01:08:06 +0200

    feat(exposed/tags): adds log

---
+++ src/exposed/tags/log.ts
@@ -0,0 +1,18 @@
+import { TScript } from '~/types';
+import asTag from '~/utils/as-tag';
+
+export default log;
+
+function log(message: string): TScript;
+function log(literals: TemplateStringsArray, ...placeholders: any[]): TScript;
+/**
+ * String tag; logs a string on `stdout`.
+ * @returns A `TScript`, as a function, that won't be executed until called by `kpo` -hence, calling `log` won't have any effect until the returned function is called.
+ */
+function log(...args: any[]): TScript {
+  return function log(): void {
+    const message = asTag(args.shift(), ...args);
+    // eslint-disable-next-line no-console
+    console.log(message);
+  };
+}
