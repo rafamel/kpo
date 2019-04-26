aba9763d0f4e065c8603b19a456d32d09032d247
Rafa Mel <melrcm@gmx.net>
2019-04-27 01:07:44 +0200

    feat(exposed/tags): adds rm

--- src/exposed/tags/index.ts
+++ src/exposed/tags/index.ts
@@ -1,4 +1,5 @@
 export { default as ensure } from './ensure';
 export { default as exists } from './exists';
 export { default as line } from './line';
+export { default as rm } from './rm';
 export { default as silent } from './silent';
---
+++ src/exposed/tags/rm.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import fs from 'fs-extra';
+import core from '~/core';
+import asTag from '~/utils/as-tag';
+import { TScript } from '~/types';
+import { rejects } from 'errorish';
+
+export default rm;
+
+function rm(path: string): TScript;
+function rm(literals: TemplateStringsArray, ...placeholders: any[]): TScript;
+/**
+ * String tag; recursively removes `path`, which can be relative to the project's directory.
+ * @returns A `TScript`, as a function, that won't be executed until called by `kpo` -hence, calling `rm` won't have any effect until the returned function is called.
+ */
+function rm(...args: any[]): TScript {
+  return async function rm(): Promise<void> {
+    let file = asTag(args.shift(), ...args);
+    if (!path.isAbsolute(file)) {
+      const paths = await core.paths();
+      file = path.join(paths.directory, file);
+    }
+
+    await fs.remove(file).catch(rejects);
+  };
+}
