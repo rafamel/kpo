bb4baa7f3b75d7b2fba54ef194cd742ed255e18f
Rafa Mel <melrcm@gmx.net>
2019-04-28 04:38:09 +0200

    refactor(tags/rm): uses file/remove

--- src/exposed/tags/rm.ts
+++ src/exposed/tags/rm.ts
@@ -1,11 +1,7 @@
-import path from 'path';
-import fs from 'fs-extra';
-import core from '~/core';
 import asTag from '~/utils/as-tag';
-import { rejects } from 'errorish';
 import expose from '~/utils/expose';
+import remove from '../file/remove';
 
-// TODO delete rm
 export default expose(rm);
 
 function rm(path: string): () => Promise<void>;
@@ -14,18 +10,13 @@ function rm(
   ...placeholders: any[]
 ): () => Promise<void>;
 /**
- * String tag; recursively removes `path`, which can be relative to the project's directory.
+ * String tag; recursively removes `path`, which can be relative to the project's directory. It won't ask for user confirmation, nor fail if `path` doesn't exist.
  * It is an *exposed* function: call `rm.fn()`, which takes the same arguments, in order to execute on call.
  * @returns An asynchronous function -hence, calling `rm` won't have any effect until the returned function is called.
  */
 function rm(...args: any[]): () => Promise<void> {
   return async () => {
     let file = asTag(args.shift(), ...args);
-    if (!path.isAbsolute(file)) {
-      const paths = await core.paths();
-      file = path.join(paths.directory, file);
-    }
-
-    await fs.remove(file).catch(rejects);
+    return remove.fn(file);
   };
 }
