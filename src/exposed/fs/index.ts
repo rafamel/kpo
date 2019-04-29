0acf5a5c67dc348010dd8a1f767da0d24ae59cb7
Rafa Mel <melrcm@gmx.net>
2019-04-29 07:56:45 +0200

    feat(exposed/fs): adds move

--- src/exposed/fs/index.ts
+++ src/exposed/fs/index.ts
@@ -2,7 +2,6 @@ export { default as copy } from './copy';
 export { default as json } from './json';
 export { default as remove } from './remove';
 export { default as mkdir } from './mkdir';
+export { default as move } from './move';
 export { default as rw } from './rw';
 export { default as write } from './write';
-
-// TODO:  move
---
+++ src/exposed/fs/move.ts
@@ -0,0 +1,58 @@
+import path from 'path';
+import fs from 'fs-extra';
+import core from '~/core';
+import { absolute, exists } from '~/utils/file';
+import { IFsWriteOptions } from './types';
+import expose from '~/utils/expose';
+import confirm from './utils/confirm';
+import { rejects } from 'errorish';
+import logger from '~/utils/logger';
+
+export default expose(move);
+
+/**
+ * Move files or directories.
+ * It is an *exposed* function: call `move.fn()`, which takes the same arguments, in order to execute on call.
+ * @returns An asynchronous function -hence, calling `move` won't have any effect until the returned function is called.
+ */
+function move(
+  src: string,
+  dest: string,
+  options: IFsWriteOptions = {}
+): () => Promise<void> {
+  return async () => {
+    options = Object.assign({ overwrite: true }, options);
+
+    const cwd = await core.cwd();
+    src = absolute({ path: src, cwd });
+    dest = absolute({ path: dest, cwd });
+
+    const relatives = {
+      src: path.relative(src, cwd),
+      dest: path.relative(dest, cwd)
+    };
+
+    const srcExist = await exists(src, { fail: options.fail });
+    if (!srcExist) return;
+
+    const destExists = await exists(dest);
+    if (destExists) {
+      if (options.fail) {
+        throw Error(`Destination already exists: ${relatives.dest}`);
+      }
+      if (!options.overwrite) return;
+    }
+
+    if (
+      !(await confirm(
+        `Move "${relatives.src}" to "${relatives.dest}"?`,
+        options
+      ))
+    ) {
+      return;
+    }
+
+    await fs.move(src, dest, { overwrite: options.overwrite }).catch(rejects);
+    logger.info(`Moved "${relatives.src}" to "${relatives.dest}"`);
+  };
+}
