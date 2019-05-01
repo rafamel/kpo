10bd3eb1b4b1481f5f7c5348b6dc2f994329bcb0
Rafa Mel <melrcm@gmx.net>
2019-05-01 10:07:32 +0200

    refactor(public/kpo): minor changes to logged messages for raise

--- src/public/kpo/raise.ts
+++ src/public/kpo/raise.ts
@@ -75,7 +75,7 @@ function raise(options: IRaiseOptions = {}): () => Promise<void> {
       .filter((key) => !taskNames.includes(key))
       .concat(Object.keys(scripts).filter((key) => toAdd.includes(key)));
 
-    let msg = chalk.bold.green('No pending scripts changes');
+    let msg = chalk.bold('No pending scripts changes');
     if (toRemove.length || toAdd.length) {
       msg = toRemove.length
         ? chalk.bold.yellow('Scripts to remove: ') + toRemove.join(', ')
