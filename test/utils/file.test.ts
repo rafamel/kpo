3827399424ae7e78261efbafea9c1e4d92307b22
Rafa Mel <melrcm@gmx.net>
2019-05-02 03:54:42 +0200

    test(utils/file): tests absolute

--- test/utils/file.test.ts
+++ test/utils/file.test.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import up from 'find-up';
-import { find, exists } from '~/utils/file';
+import { absolute, exists, find } from '~/utils/file';
 import { IOfType } from '~/types';
 
 jest.mock('find-up');
@@ -14,6 +14,29 @@ const at = (str?: string): string => {
   return path.join(__dirname, '../fixtures', str || '');
 };
 
+describe(`absolute`, () => {
+  test(`succeeds`, () => {
+    expect(absolute({ path: 'bar/baz', cwd: '/foo' })).toBe('/foo/bar/baz');
+    expect(absolute({ path: './bar/baz', cwd: '/foo' })).toBe('/foo/bar/baz');
+    expect(absolute({ path: '/bar/baz', cwd: '/foo' })).toBe('/bar/baz');
+  });
+});
+describe(`exists`, () => {
+  test(`succeeds w/ file`, async () => {
+    await expect(exists(at('js/kpo.scripts.js'))).resolves.toBe(true);
+    await expect(exists(at('js/kpo.scripts.js'), { fail: true })).resolves.toBe(
+      true
+    );
+  });
+  test(`succeeds w/ dir`, async () => {
+    await expect(exists(at('js'))).resolves.toBe(true);
+    await expect(exists(at('js'), { fail: true })).resolves.toBe(true);
+  });
+  test(`fails`, async () => {
+    await expect(exists(at('foo'))).resolves.toBe(false);
+    await expect(exists(at('foo'), { fail: true })).rejects.toThrowError();
+  });
+});
 describe(`find`, () => {
   describe(`!strict`, () => {
     test(`succeeds`, async () => {
@@ -62,21 +85,5 @@ describe(`find`, () => {
         find(['foo.bar.js', 'bar.foo.js'], at('js/kpo.scripts.js'), true)
       ).rejects.toThrowError();
     });
-  });
-});
-describe(`exists`, () => {
-  test(`succeeds w/ file`, async () => {
-    await expect(exists(at('js/kpo.scripts.js'))).resolves.toBe(true);
-    await expect(exists(at('js/kpo.scripts.js'), { fail: true })).resolves.toBe(
-      true
-    );
-  });
-  test(`succeeds w/ dir`, async () => {
-    await expect(exists(at('js'))).resolves.toBe(true);
-    await expect(exists(at('js'), { fail: true })).resolves.toBe(true);
-  });
-  test(`fails`, async () => {
-    await expect(exists(at('foo'))).resolves.toBe(false);
-    await expect(exists(at('foo'), { fail: true })).rejects.toThrowError();
   });
 });
