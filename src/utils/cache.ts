fad3fd6dc8ac1b6c2b0fa64347cdf1fcbd55b606
Rafa Mel <melrcm@gmx.net>
2019-05-06 11:16:06 +0200

    feat(utils/cache): allows for getId to be null or return string or number types

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,9 +1,12 @@
-export default function cache<T>(getId: () => string, fn: () => T): () => T {
-  let id: null | string = null;
+export default function cache<T>(
+  getId: (() => string | number) | void | null,
+  fn: () => T
+): () => T {
+  let id: null | string | number = null;
   let value: null | T = null;
 
   return function() {
-    const key = getId();
+    const key = getId ? getId() : '_';
     if (key === id) return value as T;
 
     id = key;
