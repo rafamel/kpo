c90f121711d39b7140fc038e9a1e8b371f82764c
Rafa Mel <melrcm@gmx.net>
2019-05-06 13:05:17 +0200

    feat(utils/ps-manager): adds child processes manager

---
+++ src/utils/ps-manager.ts
@@ -0,0 +1,30 @@
+import globals from '~/globals';
+import { IOfType } from '~/types';
+import logger from './logger';
+
+const processes: IOfType<Promise<void>> = globals('processses', {}).get();
+
+function remove(pid: number): void {
+  try {
+    delete processes[pid];
+  } catch (_) {
+    logger.error(`Removal of child process ${pid} failed`);
+  }
+}
+
+export default {
+  add(pid: number, promise: Promise<void>): void {
+    processes[pid] = promise.then(() => remove(pid)).catch(() => remove(pid));
+  },
+  kill(signal: string): void {
+    Object.keys(processes)
+      .map(Number)
+      .forEach((pid) => process.kill(pid, signal));
+  },
+  isDone(): boolean {
+    return !Object.keys(processes).length;
+  },
+  async promise(): Promise<void> {
+    await Promise.all(Object.values(processes));
+  }
+};
