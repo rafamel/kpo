25062640dd6c5dafc0a3521f0e650496f2a4a587
Rafa Mel <melrcm@gmx.net>
2019-05-06 14:45:21 +0200

    refactor(bin): defers error message formatting to logger

--- src/bin/kpo.ts
+++ src/bin/kpo.ts
@@ -2,7 +2,6 @@
 
 import main from './main';
 import { ensure } from 'errorish';
-import { error } from 'cli-belt';
 import errors from '~/utils/errors';
 import logger from '~/utils/logger';
 import { terminate, state } from 'exits';
@@ -19,14 +18,8 @@ main(process.argv.slice(2)).catch(async (err) => {
   if (state().triggered) return;
 
   err = ensure(err);
-  const isOpen = err instanceof errors.OpenError;
-  error(err, {
-    debug: true,
-    logger: {
-      error: logger.error,
-      debug: isOpen ? logger.error : logger.debug
-    }
-  });
+  logger.error(err.message);
+  if (err instanceof errors.OpenError) logger.debug(err);
 
   return terminate('exit', 1);
 });
