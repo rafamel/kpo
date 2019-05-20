cf99127aa5cef28992f544642d0d0e64875688b9
Rafa Mel <melrcm@gmx.net>
2019-05-20 03:30:19 +0200

    feat(fs/tags): globs is an exposed function
    BREAKING CHANGE: Glob previously returned a promise; it now returns a promise returning function

--- src/public/tags/glob.ts
+++ src/public/tags/glob.ts
@@ -1,27 +1,35 @@
-import { error } from '~/utils/errors';
 import asTag from '~/utils/as-tag';
+import expose, { TExposedOverload } from '~/utils/expose';
 import _glob from 'glob';
+import { error } from '~/utils/errors';
 
-export default glob;
+export default expose(glob) as TExposedOverload<
+  typeof glob,
+  [string] | [TemplateStringsArray, ...any[]]
+>;
 
-function glob(pattern: string): Promise<string[]>;
+function glob(pattern: string): () => Promise<string[]>;
 function glob(
   literals: TemplateStringsArray,
   ...placeholders: any[]
-): Promise<string[]>;
+): () => Promise<string[]>;
+
 /**
  * String tag; returns a promise resolving with all paths matching a glob
- * @returns Promise<string[]>
+ * It is an *exposed* function: use `glob.fn` as tag instead in order to execute on call.
+ * @returns A function -hence, calling `glob` won't have any effect until the returned function is called.
  */
-async function glob(...args: any[]): Promise<string[]> {
-  try {
-    const pattern = asTag(args.shift(), ...args);
-    return await new Promise((resolve: (arg: string[]) => void, reject) =>
-      _glob(pattern, { cwd: process.cwd() }, (err, matches) =>
-        err ? reject(err) : resolve(matches)
-      )
-    );
-  } catch (err) {
-    throw error(err);
-  }
+function glob(...args: any[]): () => Promise<string[]> {
+  return async () => {
+    try {
+      const pattern = asTag(args.shift(), ...args);
+      return await new Promise((resolve: (arg: string[]) => void, reject) =>
+        _glob(pattern, { cwd: process.cwd() }, (err, matches) =>
+          err ? reject(err) : resolve(matches)
+        )
+      );
+    } catch (err) {
+      throw error(err);
+    }
+  };
 }
