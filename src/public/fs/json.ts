7e6e89f8957619807f4b006110139b2b8da94ef5
Rafa Mel <melrcm@gmx.net>
2019-06-11 15:01:43 +0200

    fix(public): fixes json typings

--- src/public/fs/json.ts
+++ src/public/fs/json.ts
@@ -10,7 +10,7 @@ import {
 } from './types';
 
 export default expose(json) as TExposedOverload<
-  typeof rw,
+  typeof json,
   | [TSource, TDestination, TJsonFn, IFsUpdateOptions]
   | [TSource, TDestination, TJsonFn]
   | [TSource, TJsonFn, IFsUpdateOptions]
