192ed1c63725cac3b9aab7afbc043f25505d2cd0
Rafa Mel <melrcm@gmx.net>
2021-02-17 14:10:57 +0100

    feat(tasks): adds move task

--- src/tasks/create/index.ts
+++ src/tasks/create/index.ts
@@ -2,6 +2,7 @@ export * from './clear';
 export * from './copy';
 export * from './exec';
 export * from './log';
+export * from './move';
 export * from './print';
 export * from './raises';
 export * from './sleep';
