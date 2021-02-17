89f1d1f638888c5bbe1d4ae15ae123741a643e26
Rafa Mel <melrcm@gmx.net>
2021-02-17 13:28:36 +0100

    feat(definitions): adds initial definitions

---
+++ src/definitions/index.ts
@@ -0,0 +1,29 @@
+import { Members } from 'type-core';
+import { Writable, Readable } from 'stream';
+
+export type Task = (context: Context) => Promise<void> | void;
+
+export declare namespace Task {
+  export type Sync = (context: Context) => void;
+  export type Async = (context: Context) => Promise<void>;
+  export interface Record {
+    [key: string]: Task | Record;
+  }
+}
+
+export interface Context {
+  readonly cwd: string;
+  readonly env: Members<string | undefined>;
+  readonly args: string[];
+  readonly stdio: Stdio;
+  readonly level: LogLevel;
+  readonly route: Array<string | number>;
+  readonly prefix: PrefixPolicy;
+  readonly cancellation: Promise<void>;
+}
+
+export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'silent';
+
+export type PrefixPolicy = 'none' | 'print' | 'exec' | 'all';
+
+export type Stdio = [Readable, Writable, Writable];
