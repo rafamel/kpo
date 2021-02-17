8535df8a20c4a1b8c084b1d9cb903c3b246ab5a3
Rafa Mel <melrcm@gmx.net>
2021-02-17 13:30:23 +0100

    feat(utils): adds isCancelled

---
+++ src/utils/is-cancelled.ts
@@ -0,0 +1,13 @@
+import { Context } from '../definitions';
+
+/**
+ * A promise returning function that will
+ * resolve with `true` if `context.cancellation`
+ * has finalized, and `false` otherwise.
+ */
+export async function isCancelled(context: Context): Promise<boolean> {
+  return new Promise<boolean>((resolve) => {
+    context.cancellation.finally(() => resolve(true));
+    setTimeout(() => resolve(false), 0);
+  });
+}
