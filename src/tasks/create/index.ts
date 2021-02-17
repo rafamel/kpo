1964dc8c11c34ad0b555fa5db401271b448c19c4
Rafa Mel <melrcm@gmx.net>
2021-02-17 13:49:42 +0100

    feat(tasks): adds sleep task

--- src/tasks/create/index.ts
+++ src/tasks/create/index.ts
@@ -1,3 +1,4 @@
 export * from './clear';
 export * from './log';
 export * from './print';
+export * from './sleep';
---
+++ src/tasks/create/sleep.ts
@@ -0,0 +1,17 @@
+import { Task, Context } from '../../definitions';
+import { log } from './log';
+import { into } from 'pipettes';
+
+export function sleep(ms: number): Task.Async {
+  return async (ctx: Context): Promise<void> => {
+    into(ctx, log('debug', `Sleep for ${ms}ms`));
+
+    return new Promise((resolve) => {
+      const timeout = setTimeout(() => resolve(), ms);
+      ctx.cancellation.finally(() => {
+        clearTimeout(timeout);
+        resolve();
+      });
+    });
+  };
+}
