4494630f5267ef07b0377d405015d866da85c5ee
Rafa Mel <melrcm@gmx.net>
2021-02-17 13:44:25 +0100

    feat(tasks): adds context transform

---
+++ src/tasks/transform/context.ts
@@ -0,0 +1,16 @@
+import { Task, Context } from '../../definitions';
+import { run } from '../consume/run';
+import { UnaryFn, Empty } from 'type-core';
+
+export function context(
+  context:
+    | Empty
+    | Partial<Context>
+    | UnaryFn<Context, Partial<Context> | Empty>,
+  task: Task
+): Task.Async {
+  const fn = typeof context === 'function' ? context : () => context;
+  return async (context: Context): Promise<void> => {
+    await run(task, { ...context, ...fn(context) });
+  };
+}
