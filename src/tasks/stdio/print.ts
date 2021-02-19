df08e299372ea45104186178aea29a879cc1eca8
Rafa Mel <melrcm@gmx.net>
2021-02-19 18:47:35 +0100

    feat(tasks): print can take no arguments

--- src/tasks/stdio/print.ts
+++ src/tasks/stdio/print.ts
@@ -2,10 +2,10 @@ import { Task, Context } from '../../definitions';
 import { addPrefix } from '../../helpers/prefix';
 import util from 'util';
 
-export function print(item: any, ...data: any[]): Task.Sync {
+export function print(...data: any[]): Task.Sync {
   return (ctx: Context): void => {
     const str = addPrefix(
-      util.format(item, ...data) + '\n',
+      data.length ? util.format(data[0], ...data.slice(1)) + '\n' : '\n',
       null,
       'print',
       ctx
