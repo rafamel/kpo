f0dbd5aad88a443e714352d81788f282ac48b699
Rafa Mel <melrcm@gmx.net>
2021-02-19 18:49:36 +0100

    feat(tasks): raises can take a string as an argument

--- src/tasks/process/raises.ts
+++ src/tasks/process/raises.ts
@@ -1,8 +1,11 @@
 import { Task, Context } from '../../definitions';
 import { UnaryFn } from 'type-core';
 
-export function raises(error: Error | UnaryFn<Context, Error>): Task.Sync {
+export function raises(
+  error: Error | string | UnaryFn<Context, Error | string>
+): Task.Sync {
   return (ctx: Context): void => {
-    throw typeof error === 'function' ? error(ctx) : error;
+    const err = typeof error === 'function' ? error(ctx) : error;
+    throw typeof err === 'string' ? Error(err) : err;
   };
 }
