6bcb47e28b1d135129dcf672e50a109a22824107
Rafa Mel <melrcm@gmx.net>
2021-02-19 18:55:05 +0100

    feat(tasks): list prints the command for tasks and their indented structure

--- src/tasks/reflection/list.ts
+++ src/tasks/reflection/list.ts
@@ -2,17 +2,35 @@ import { Task, Context } from '../../definitions';
 import { parseToArray } from '../../helpers/parse';
 import { print } from '../stdio/print';
 import { into } from 'pipettes';
+import table from 'as-table';
 import chalk from 'chalk';
 
 export function list(tasks: Task.Record): Task.Sync {
   return (ctx: Context): void => {
-    const str = parseToArray(tasks)
-      .map((item) => {
-        return ' '.repeat((item.route.length - 1) * 2) + item.name;
-      })
-      .map((str) => chalk.bold(str))
-      .join('\n');
+    const items = parseToArray(tasks);
+    const maxRouteLength = items.reduce(
+      (acc, item) => (acc > item.route.length ? acc : item.route.length),
+      0
+    );
 
-    into(ctx, print(str));
+    const rows = items.map((item) => {
+      return [
+        'kpo ',
+        chalk.bold(item.name),
+        ' '.repeat(6),
+        ...Array(item.route.length).fill(''),
+        item.route[item.route.length - 1],
+        ...Array(maxRouteLength - item.route.length).fill('')
+      ];
+    });
+
+    into(
+      ctx,
+      print(
+        table
+          .configure({ delimiter: '' })(rows)
+          .trim()
+      )
+    );
   };
 }
