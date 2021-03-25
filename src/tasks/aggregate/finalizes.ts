581461ce85fc3ec4ecfd87e67cbe3862a3ac925f
Rafa Mel <melrcm@gmx.net>
2021-03-25 08:16:38 +0100

    feat(tasks): adds finalizes transform task

---
+++ src/tasks/aggregate/finalizes.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-useless-catch */
+import { Task, Context } from '../../definitions';
+
+/**
+ * Always executes a `final` task after another.
+ * If the first throws an exception and the second
+ * does not, it will finally throw the initial exception.
+ * @returns Task
+ */
+export function finalizes(task: Task, final?: Task | null): Task.Async {
+  return async (ctx: Context): Promise<void> => {
+    try {
+      await task(ctx);
+    } catch (err) {
+      throw err;
+    } finally {
+      if (final) await final(ctx);
+    }
+  };
+}
--- src/tasks/aggregate/index.ts
+++ src/tasks/aggregate/index.ts
@@ -1,4 +1,5 @@
 export * from './combine';
+export * from './finalizes';
 export * from './parallel';
 export * from './select';
 export * from './series';
