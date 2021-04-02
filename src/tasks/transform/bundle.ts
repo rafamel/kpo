241de8ab2efda7edb006e88cb445ea5d6a64cc5b
Rafa Mel <melrcm@gmx.net>
2021-04-02 12:20:21 +0200

    feat(tasks): add bundle task

---
+++ src/tasks/transform/bundle.ts
@@ -0,0 +1,18 @@
+import { Task, Context } from '../../definitions';
+import { run } from '../../utils/run';
+import { Empty, UnaryFn } from 'type-core';
+
+/**
+ * Takes an optionally *Task* returning function
+ * and turns the function itself and the returned
+ * *Task* into a single sequential *Task*.
+ * @returns Task
+ */
+export function bundle(
+  fn: UnaryFn<Context, Task | Empty | Promise<Task | Empty>>
+): Task.Async {
+  return async (ctx: Context): Promise<void> => {
+    const task = await fn(ctx);
+    if (task) await run(task, ctx);
+  };
+}
--- src/tasks/transform/index.ts
+++ src/tasks/transform/index.ts
@@ -1,3 +1,4 @@
+export * from './bundle';
 export * from './catches';
 export * from './context';
 export * from './final';
