f9d9b273f8875ca797ea628fd8484e3f42379de7
Rafa Mel <melrcm@gmx.net>
2021-04-04 13:37:13 +0200

    refactor: remove dangling objectpath type definition

--- src/@types/objectpath.ts
+++
@@ -1,7 +0,0 @@
-declare module 'objectpath' {
-  const ObjectPath: {
-    parse(str: string, quote?: string, forceQuote?: boolean): string[];
-    stringify(arr: string[], quote?: string, forceQuote?: boolean): string;
-  };
-  export default ObjectPath;
-}
