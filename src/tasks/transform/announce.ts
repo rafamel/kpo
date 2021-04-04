3182d0a9f319515e3e1c7ab3d0093d6a01fbf9e5
Rafa Mel <melrcm@gmx.net>
2021-04-04 13:40:55 +0200

    feat(tasks): add announce task

---
+++ src/tasks/transform/announce.ts
@@ -0,0 +1,34 @@
+import { Context, LogLevel, Task } from '../../definitions';
+import { stringifyPrintRoute } from '../../helpers/stringify-route';
+import { styleString } from '../../helpers/style-string';
+import { run } from '../../utils/run';
+import { series } from '../aggregate/series';
+import { log } from '../stdio/log';
+import { shallow } from 'merge-strategies';
+import { into } from 'pipettes';
+
+export interface AnnounceOptions {
+  level?: LogLevel;
+}
+
+/**
+ * Prints tasks route before execution.
+ * @returns Task
+ */
+export function announce(task: Task, options?: AnnounceOptions): Task.Async {
+  return async (ctx: Context): Promise<void> => {
+    const opts = shallow({ level: 'info' }, options || undefined);
+
+    return into(
+      ctx.route.length
+        ? log(
+            opts.level,
+            styleString('task |', { bold: true }),
+            stringifyPrintRoute(ctx.route)
+          )
+        : null,
+      (first) => series(first, task),
+      (task) => run(task, ctx)
+    );
+  };
+}
--- src/tasks/transform/index.ts
+++ src/tasks/transform/index.ts
@@ -1,3 +1,4 @@
+export * from './announce';
 export * from './bundle';
 export * from './catches';
 export * from './context';
