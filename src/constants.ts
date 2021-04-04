fada578878a927b828056a33f10a5ff5e1b01429
Rafa Mel <melrcm@gmx.net>
2021-04-04 16:06:50 +0200

    fix: fix type definitions build

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,14 @@
-import pkg from '../package.json';
+import { into } from 'pipettes';
 
-export const constants = {
-  bin: 'kpo',
-  file: 'kpo.tasks.js',
-  description: pkg.description || '',
-  version: pkg.version || 'Unknown',
-  record: { default: 'default' }
-};
+export const constants = into(
+  { pkg: require('../package.json') },
+  ({ pkg }) => ({
+    bin: 'kpo',
+    file: 'kpo.tasks.js',
+    description: pkg.description || '',
+    version: pkg.version || 'Unknown',
+    record: {
+      default: 'default'
+    }
+  })
+);
