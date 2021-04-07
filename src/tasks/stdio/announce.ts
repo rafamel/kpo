8463a823aecdc71ea9782e56b1a5e6b933e0de22
Rafa Mel <melrcm@gmx.net>
2021-04-07 22:26:28 +0200

    feat(tasks): optionally log task success on announce

--- src/tasks/stdio/announce.ts
+++ src/tasks/stdio/announce.ts
@@ -1,34 +1,40 @@
-import { Context, LogLevel, Task } from '../../definitions';
+import { Task } from '../../definitions';
 import { stringifyPrintRoute } from '../../helpers/stringify';
 import { style } from '../../utils/style';
-import { run } from '../../utils/run';
 import { series } from '../aggregate/series';
+import { create } from '../creation/create';
 import { log } from '../stdio/log';
 import { shallow } from 'merge-strategies';
-import { into } from 'pipettes';
 
 export interface AnnounceOptions {
-  level?: LogLevel;
+  info: boolean;
+  success: boolean;
 }
 
 /**
- * Prints tasks route before execution.
+ * Prints tasks route before execution and upon success.
  * @returns Task
  */
 export function announce(task: Task, options?: AnnounceOptions): Task.Async {
-  return async (ctx: Context): Promise<void> => {
-    const opts = shallow({ level: 'info' }, options || undefined);
+  return create((ctx) => {
+    const opts = shallow({ info: true, success: true }, options || undefined);
 
-    return into(
-      ctx.route.length
+    return series(
+      opts.info
         ? log(
-            opts.level,
-            style('task |', { bold: true }),
+            'info',
+            style('task', { bold: true }),
             stringifyPrintRoute(ctx.route)
           )
         : null,
-      (first) => series(first, task),
-      (task) => run(task, ctx)
+      task,
+      opts.success
+        ? log(
+            'success',
+            style('task', { bold: true }),
+            stringifyPrintRoute(ctx.route)
+          )
+        : null
     );
-  };
+  });
 }
