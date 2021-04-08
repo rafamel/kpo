6b32f6bebaa23f60cdf0021f3ab3ac489d23c193
Rafa Mel <melrcm@gmx.net>
2021-04-08 13:40:13 +0200

    feat(tasks): add name option for announce

--- src/tasks/stdio/announce.ts
+++ src/tasks/stdio/announce.ts
@@ -5,10 +5,15 @@ import { series } from '../aggregate/series';
 import { create } from '../creation/create';
 import { log } from '../stdio/log';
 import { shallow } from 'merge-strategies';
+import { TypeGuard } from 'type-core';
 
 export interface AnnounceOptions {
-  info: boolean;
-  success: boolean;
+  /** Use instead of the task route */
+  name?: string;
+  /** Print route before execution */
+  info?: boolean;
+  /** Print route after successful execution */
+  success?: boolean;
 }
 
 /**
@@ -18,23 +23,14 @@ export interface AnnounceOptions {
 export function announce(task: Task, options?: AnnounceOptions): Task.Async {
   return create((ctx) => {
     const opts = shallow({ info: true, success: true }, options || undefined);
+    const name = TypeGuard.isString(opts.name)
+      ? opts.name
+      : stringifyPrintRoute(ctx.route);
 
     return series(
-      opts.info
-        ? log(
-            'info',
-            style('task', { bold: true }),
-            stringifyPrintRoute(ctx.route)
-          )
-        : null,
+      opts.info ? log('info', style('task', { bold: true }), name) : null,
       task,
-      opts.success
-        ? log(
-            'success',
-            style('task', { bold: true }),
-            stringifyPrintRoute(ctx.route)
-          )
-        : null
+      opts.success ? log('success', style('task', { bold: true }), name) : null
     );
   });
 }
