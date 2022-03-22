f912165f08a25bf14ef574447729db1cbff9adc0
Rafa Mel <melrcm@gmx.net>
2022-03-22 10:48:52 +0000

    chore: update setup

---
+++ kpo.tasks.mjs
@@ -0,0 +1,30 @@
+import riseup from './riseup.config.mjs';
+
+export default ({ recreate, context, create, series, lift, exec, catches }) => {
+  const tasks = {
+    node: riseup.tasks.node,
+    build: riseup.tasks.build,
+    tarball: riseup.tasks.tarball,
+    docs: riseup.tasks.docs,
+    fix: riseup.tasks.fix,
+    lint: series(riseup.tasks.lintmd, riseup.tasks.lint),
+    test: riseup.tasks.test,
+    commit: riseup.tasks.commit,
+    release: context({ args: ['--no-verify'] }, riseup.tasks.release),
+    distribute: riseup.tasks.distribute,
+    validate: series(
+      create(() => tasks.lint),
+      create(() => tasks.test),
+      lift({ purge: true, mode: 'audit' }, () => tasks),
+      catches({ level: 'silent' }, exec('npm', ['outdated']))
+    ),
+    /* Hooks */
+    version: series(
+      create(() => tasks.validate),
+      create(() => tasks.build),
+      create(() => tasks.docs)
+    )
+  };
+
+  return recreate({ announce: true }, tasks);
+};
